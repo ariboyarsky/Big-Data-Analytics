5b4c0017c4c46949e82ac38071db95449d617abb
Ari Boyarsky <ariboyarsky@gwmail.gwu.edu>
2016-12-16 18:08:27 -0500

    add documentation, and part c isaac

--- Class-Project-3/Text Analytics/PartC.R
+++ Class-Project-3/Text Analytics/PartC.R
@@ -1,17 +1,25 @@
-library(tm)
-data(acq)
+install.packages("quanteda")
+library(quanteda)
 
-# search for a needle (word) in a hay stack (corpora)
-searchCorpora <- function(hay, needle){
-  for(a in hay){
-    
-  }
+doc_search <- function(data, search){
+  mycorpus <- corpus(data)
+  result <- kwic(mycorpus, search, window = 3)
+  result[[1]] <- gsub("reut-000", "", result[[1]])  
+  result[[1]] <- gsub(".xml", "", result[[1]]) 
+  doc_number <- as.numeric(result[[1]]) # document number
+  word_index <- result[[2]] # word index in document
+  word <- result[[4]][[1]] # word searching for
+  line_index <- list()
+  docs <- unique(doc_number)
+  out<-capture.output(cat(mycorpus[docs]))
+  line <- grep(search, out, ignore.case = T)
+  line_index <- list(line_index, as.numeric(line))
+  df = data.frame(doc_number,word_index, line)
+  word_used = cat("Searching documents for:", word, "\n")
+  return(df)
 }
 
-for(a in seq(acq)){
-  if(length(grep("any future", acq[[a]]$content)) > 0){
-    print(a)
-    x <- grep("any future", acq[[a]]$content)
-    x
-  }
-}
+doc_search(acq, "because")
+doc_search(acq, "Mattress")
+doc_search(acq, "appraisals")
+ 
